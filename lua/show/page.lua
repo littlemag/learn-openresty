75d790fd1b95064851110240c55b4e02a28f7d69
brad <252141638@qq.com>
2018-12-29 17:02:30 +0800

    show page

--- lua/show/page.lua
+++ lua/show/page.lua
@@ -1,4 +1,4 @@
 local template = require "resty.template"
 local args = ngx.req.get_uri_args()
 local page = ngx.var.path
-template.render(page ..".html",{param = args.param1,param2 = args.param2})
+template.render("/usr/local/openresty/nginx/lua/learn-openresty/lua/tpl/page.html",{param = args.param1,param2 = args.param2})
